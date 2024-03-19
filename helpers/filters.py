4ace67afdaf24e7270d1796e030cd48b212b40b2
ogrehere <163102104+ogrehere@users.noreply.github.com>
2024-03-19 20:16:43 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -5,7 +5,11 @@ from pyrogram import filters
 from config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
-other_filters2 = filters.private & ~filters.via_bot & ~filters.forwarded
+try:
+    other_filters2 = filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+except AttributeError:
+    other_filters2 = filters.private & ~filters.via_bot & ~filters.forwarded
+
 
 
 def command(commands: Union[str, List[str]]):
